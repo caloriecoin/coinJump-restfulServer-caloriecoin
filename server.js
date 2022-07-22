b38fac824f9e69a95c06abcb1e468208d7519e12
Karriz <karriz@xymbool.com>
2022-07-22 21:06:43 +0900

    remove socket io

--- server.js
+++ server.js
@@ -32,105 +32,6 @@ app.get("/", (req, res) => {
 
 //03.socket.io (줄넘기 배틀용) 따로 만듬
 const socketServer = require("http").createServer(app);
-const io = require("socket.io")(socketServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-const players = [];
-const player1 = "";
-const player2 = "";
-const player1Jumps = [];
-const player2Jumps = [];
-const player1LastJumpCount = 0;
-const player2LastJumpCount = 0;
-const winner = "";
-const drawer = [];
-const looser = "";
-
-io.on("connection", (socket) => {
-  console.log("connection made successfully");
-
-  socket.on("enterBattleRoom", (kakaoId) => {
-    //B. 방이 꽉차서 못들어가게 해주세요.
-
-    if (players.length == 2) {
-      socket.emit("gameStart", { msg: "게임중" });
-    } else if (players.length > 2) {
-      socket.emit("overRoom", (msg) => {
-        msg: "방이 꽉찼어요. 입장불가";
-      });
-      return socket.emit("disconnect");
-    } else {
-      players.push(kakaoId);
-      console.log(players);
-    }
-
-    //C.배틀방에 들어온 순서대로 카카오 아이디값을 배열에 두고 먼저 들어온 순서대로 1p, 2p를 잡음
-
-    if (players[0] == kakaoId) {
-      player1 = kakaoId;
-      socket.emit("youPlayer1", { msg: "당신은 1번 플레이어 입니다." });
-    } else {
-      player2 = kakaoId;
-      socket.emit("youPlayer2", { msg: "당신은 2번 플레이어 입니다." });
-    }
-  });
-
-  // 유저의 줄넘기 데이터 받음
-  socket.on("userJump", (jumps) => {
-    // 내 소켓 줄넘기는 안받음, 다른사람 줄넘기 정보만 받음
-    socket.broadcast.emit("otherUserJump", jumps);
-
-    //만약에 플레이어1 이면 플레이어1 점프스에 기록 넣는다.
-    if (jumps.player == player1) {
-      player1Jumps.push(jumps);
-      player1LastJumpCount = jumps.jumps;
-    } else {
-      //만약에 플레이어2 이면 플레이어2 점프스에 기록 넣는다.
-      player2Jumps.push(jumps);
-      player2LastJumpCount = jumps.jumps;
-    }
-
-    console.log(jumps);
-  });
-
-  socket.on("gameOver", () => {
-    if (player1LastJumpCount > player2LastJumpCount) {
-      winner = player1;
-      looser = player2;
-    } else if (player1LastJumpCount == player2LastJumpCount) {
-      drawer = [player1, player2];
-    } else {
-      winner = player2;
-      looser = player1;
-    }
-
-    //해당 데이터 모두 디비로 포스트함 ??????
-
-    //소켓들을 강제로 접속을 끊고 방에서 나가게한다. ??????
-
-    //서버의 배열을 다시 깨끗히 비워주세요.
-    players = [];
-    player1 = "";
-    player2 = "";
-    player1Jumps = [];
-    player2Jumps = [];
-    player1LastJumpCount;
-    player2LastJumpCount;
-    winner;
-    drawer = [];
-    looser;
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected", kakaoId);
-    players.pop(kakaoId);
-    console.log(players);
-  });
-});
 
 //04. 소켓서버랑 중앙서버 둘다 실행
 app.listen(
@@ -138,7 +39,4 @@ app.listen(
   console.log(
     `칼로리코인(런메이트) 중앙서버작동 => by ${process.env.NODE_ENV} , http:localhost:${process.env.PORT}`
   )
-);
-socketServer.listen(process.env.SOCKET_PORT, () =>
-  console.log("줄넘기 배틀 전용 소켓 서버 실행중")
 );
