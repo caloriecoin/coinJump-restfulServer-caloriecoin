fc62ac584c11c8d1c2e8db738453b73dc7717c09
Karriz <karriz@xymbool.com>
2022-07-26 00:45:08 +0900

    fix/edit-id-type-string-to-object

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ProductSchema = new mongoose.Schema(
   {
-    _id: { type: String, required: true, unique: true, immutable: true },
+    _id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      immutable: true,
+    },
     product_category: { type: String },
     product_thumbNail: { type: String },
     product_name: { type: String },
